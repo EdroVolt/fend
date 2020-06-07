4f607e6faaf9e0c4a7090c5f851480677f925882
Edro <ahmededrees30@gmail.com>
2020-06-07 23:21:18 +0200

    feat: add dynamically updated UI with weather of a city by zipcode which added by the user using third party API

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,48 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`server running on port: ${port}`);
+}
+
+// GET: data
+app.get('/data', retriveData);
+
+function retriveData(req, res) {
+    res.send(projectData);
+}
+
+// POST: data
+app.post('/data', storeData);
+
+function storeData(req, res) {
+    projectData['temperature'] = req.body.temperature
+    projectData['date'] = req.body.date
+    projectData['userResponse'] = req.body.userResponse
+    res.send(projectData);
+}
